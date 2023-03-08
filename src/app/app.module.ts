06fd1137559a0a25bcdfd1a81f7aecbabeed1157
otienochris <otienochris98@gmail.com>
2023-03-08 17:24:36 +0300

    adding different pages like about, home, dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,18 @@ import { AboutComponent } from './pages/about/about.component';
 import { AdminDashboardComponent } from './pages/admin-dashboard/admin-dashboard.component';
 import { UserDashboardComponent } from './pages/user-dashboard/user-dashboard.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import { MainNavBarComponent } from './components/main-nav-bar/main-nav-bar.component';
+import {RouterModule} from "@angular/router";
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+
+const routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'user', component: UserDashboardComponent},
+  {path: 'admin', component: AdminDashboardComponent},
+  {path: '**', component: PageNotFoundComponent},
+]
 
 @NgModule({
   declarations: [
@@ -14,11 +26,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     HomeComponent,
     AboutComponent,
     AdminDashboardComponent,
-    UserDashboardComponent
+    UserDashboardComponent,
+    MainNavBarComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    RouterModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/main-nav-bar/main-nav-bar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-nav-bar',
+  templateUrl: './main-nav-bar.component.html',
+  styleUrls: ['./main-nav-bar.component.css']
+})
+export class MainNavBarComponent {
+
+}
